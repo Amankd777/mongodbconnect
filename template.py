55ab216d6691895ff822eaea361603901435ec44
Amankd777 <20bds003@iiitdwd.ac.in>
2024-02-09 23:59:23 +0530

    MongoDB connect 1

---
+++ template.py
@@ -0,0 +1,37 @@
+import logging
+import os
+from pathlib import Path #telusko
+
+package_name = "mongodb_connect"
+
+list_of_files=[
+    ".github/workflows/ci.yaml",
+    "src/__init__.py",
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/mongo_crud.py,"
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/unit/unit.py",
+    "tests/integration/__init__.py",
+    "tests/integration/integrate.py"
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb"
+]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedr, filename = os.path.split(filepath)
+    if filedr != "":
+        os.makedirs(filedr, exist_ok=True)
+        logging.info(f"creating directory: {filedr} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass # create an empty file
+
